55cf27ebe32b9da853cde12cbe8508f7117a63cd
Himicat-web <cyxapuk2289902@gmail.com>
2026-01-15 16:23:16 +0700

    1
    H/W

---
+++ src/AlexanderSavon-HW/Assets/Code/Player.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Player : MonoBehaviour
+{
+    public float baseDamage = 55.0f; //добавил в инспектор что бы можно было регулировать
+    
+    float CritDamage;
+
+
+    void Start()
+    {
+    int Multiplier = Random.Range(1, 5); // рандом для множителя урона
+     int CritChance = Random.Range(1, 100); // рандом для шанса крит удара 
+        
+        if (CritChance >= 70)
+        {
+            CritDamage = (baseDamage * Multiplier);
+            Debug.Log("Критический урон! Нанесено: " + CritDamage);
+        }
+        
+        else
+        {
+            Debug.Log("Нанесено урона: " + baseDamage);
+        }
+    }
+
+}
