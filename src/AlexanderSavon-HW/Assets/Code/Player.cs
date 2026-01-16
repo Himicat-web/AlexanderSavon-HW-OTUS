d82cf1d1602da4b698ccb21115ffb7d824e3599b
Himicat-web <cyxapuk2289902@gmail.com>
2026-01-16 21:12:01 +0700

    испраления
    1. Форматирование кода
    2. Переставил пространства имен

--- src/AlexanderSavon-HW/Assets/Code/Player.cs
+++ src/AlexanderSavon-HW/Assets/Code/Player.cs
@@ -1,26 +1,24 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
 namespace DamageСalculation
 {
-
-    using UnityEngine;
-    using Random = UnityEngine.Random;
-
     public class Player : MonoBehaviour
     {
         public float baseDamage = 55.0f; //добавил в инспектор что бы можно было регулировать
 
-      private float CritDamage;
+        private float CritDamage;
 
-        void Start()
+       private void Start()
         {
             int Multiplier = Random.Range(2, 5); // рандом для множителя урона
-            int CritChance = Random.Range(1, 100); // рандом для шанса крит удара 
+            int CritChance = Random.Range(1, 100); // рандом для шанса крит удара
 
             if (CritChance >= 70) // сделал 30 процентов
             {
                 CritDamage = (baseDamage * Multiplier);
                 Debug.Log("Критический урон! Нанесено: " + CritDamage);
             }
-
             else
             {
                 Debug.Log("Нанесено урона: " + baseDamage);
